d31ce8310f7cd6b0cb36d190a4d7844835aeb070
Melvin Vasquez <mvasq95@hotmail.com>
2023-01-28 12:52:15 -0500

    active users rendering on chat page. need to incorporate socket.io

--- client/src/components/activeUsersBar.js
+++ client/src/components/activeUsersBar.js
@@ -1,22 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import ChannelMember from './ChannelMember';
+import axios from 'axios';
+
 
 const ActiveUsersBar = ({ socket }) => {
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
+  const [activeUsers, setActiveUsers] = useState([]);
+
+  async function getActiveUsers(){
+    try{
+      const response = await axios.get('http://localhost:3001/api/users/active');
+      setActiveUsers(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  // useEffect(() => {
+  //   socket.on('newUserResponse', (data) => setUsers(data));
+  //   console.log(users);
+  // }, [socket, users]);
 
   useEffect(() => {
-    socket.on('newUserResponse', (data) => setUsers(data));
-    console.log(users);
-  }, [socket, users]);
+    getActiveUsers();
+    console.log("Active users:")
+    console.log(activeUsers)
+  }, []);
 
   return (
     <div className="col-md-2 offset-1 jumbo-cols" id="users-col">
       <h2 className="text-center">Active</h2>
-        {users.map(function(user, index){
+        {activeUsers.map(function(user, index){
             return(
                 <ChannelMember
                   key={index}
-                  username={user.userName}
+                  username={user.user_name}
                 />
             )
         })}
